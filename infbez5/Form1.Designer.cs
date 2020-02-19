c55218aa4ae8989265f01a2655e5fe3321866518
omv0102 <omv98@outlook.com>
2020-02-19 12:59:51 +0700

    готово

--- infbez5/Form1.Designer.cs
+++ infbez5/Form1.Designer.cs
@@ -28,66 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.btn_factor = new System.Windows.Forms.Button();
+            this.txt_number = new System.Windows.Forms.NumericUpDown();
+            this.txt_file_in = new System.Windows.Forms.TextBox();
+            this.btn_file = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_number)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_factor
             // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(280, 143);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 64);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Разложить\r\nна множители";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_factor.BackColor = System.Drawing.Color.OrangeRed;
+            this.btn_factor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_factor.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_factor.FlatAppearance.BorderSize = 2;
+            this.btn_factor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btn_factor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Firebrick;
+            this.btn_factor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_factor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_factor.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_factor.Location = new System.Drawing.Point(279, 123);
+            this.btn_factor.Name = "btn_factor";
+            this.btn_factor.Size = new System.Drawing.Size(172, 64);
+            this.btn_factor.TabIndex = 0;
+            this.btn_factor.TabStop = false;
+            this.btn_factor.Text = "Разложить\r\nна множители";
+            this.btn_factor.UseVisualStyleBackColor = false;
             // 
-            // numericUpDown1
+            // txt_number
             // 
-            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown1.Location = new System.Drawing.Point(450, 284);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.txt_number.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt_number.Location = new System.Drawing.Point(57, 134);
+            this.txt_number.Maximum = new decimal(new int[] {
             999999,
             0,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.txt_number.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 38);
-            this.numericUpDown1.TabIndex = 1;
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.txt_number.Name = "txt_number";
+            this.txt_number.Size = new System.Drawing.Size(120, 38);
+            this.txt_number.TabIndex = 1;
+            this.txt_number.TabStop = false;
+            this.txt_number.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_number.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
+            // 
+            // txt_file_in
+            // 
+            this.txt_file_in.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_file_in.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt_file_in.Location = new System.Drawing.Point(73, 49);
+            this.txt_file_in.Name = "txt_file_in";
+            this.txt_file_in.ReadOnly = true;
+            this.txt_file_in.Size = new System.Drawing.Size(484, 29);
+            this.txt_file_in.TabIndex = 2;
+            this.txt_file_in.TabStop = false;
+            // 
+            // btn_file
+            // 
+            this.btn_file.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_file.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_file.FlatAppearance.BorderSize = 2;
+            this.btn_file.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_file.Location = new System.Drawing.Point(390, 102);
+            this.btn_file.Name = "btn_file";
+            this.btn_file.Size = new System.Drawing.Size(155, 53);
+            this.btn_file.TabIndex = 3;
+            this.btn_file.TabStop = false;
+            this.btn_file.Text = "Выбрать файл\r\nи считать";
+            this.btn_file.UseVisualStyleBackColor = true;
+            this.btn_file.Click += new System.EventHandler(this.btn_file_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(153, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Считывание числа из файла";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Файл:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(938, 522);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.button1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(576, 522);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_file);
+            this.Controls.Add(this.txt_file_in);
+            this.Controls.Add(this.txt_number);
+            this.Controls.Add(this.btn_factor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Факторизация числа";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_number)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btn_factor;
+        private System.Windows.Forms.NumericUpDown txt_number;
+        private System.Windows.Forms.TextBox txt_file_in;
+        private System.Windows.Forms.Button btn_file;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- infbez5/Form1.cs
+++ infbez5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace infbez5
 {
@@ -15,6 +16,32 @@ namespace infbez5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // кнопка ВЫБРАТЬ ФАЙЛ
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            string tmp = "";
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Title = "Выбрать файл ..."; // Заголовок окна
+            //Начальный путь в окне - папка проекта
+            opf.InitialDirectory = Application.StartupPath;
+
+
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                // получаем путь С НАЗВАНИЕМ файла
+                tmp = opf.FileName;
+            }
+
+            if(File.Exists(tmp) == true)
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("Выбранного файла \"" + tmp + "\" не существует!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
